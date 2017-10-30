739655db3e7150cfad300f0009775473ef1b8bdf
Christopher Diep <diep.christopher@gmail.com>
2017-10-30 22:23:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ function createRock(x) {
   function moveRock() {
     rock.style.top = `${top += 2}px`
 
-     if (checkCollision() == true) {
+     if (checkCollision()rock == true) {
        endGame()
      }
 
